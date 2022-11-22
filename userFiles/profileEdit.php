e1d0f123f13fcea1ea8c9c90f5af829e5bec77e3
clintonyau <79611899+clintonyau@users.noreply.github.com>
2022-11-21 23:24:53 -0600

    Fixed broken link
    Forgot to update file name change with a link

--- userFiles/profileEdit.php
+++ userFiles/profileEdit.php
@@ -1,187 +1,103 @@
-<?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-session_start();
-
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'group7');
-
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+<?php include ('profileEditBackend.php')?>
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border: 1px solid;
 }
+</style>
+<body>
+<?php echo "<p>Logged in as: " . $_SESSION["username"] . "</p>";?>
+<?php echo $student_table; ?>
+</br>
+<form method="post" action="profileEdit.php">
+	<label>Change your first name here</label>
+	<input type="text" required name="fname">
+	<button type="submit" class="btn" name="edit_fname">Submit</button>
+</form>
+<form method="post" action="profileEdit.php">
+	<label>Change your last name here</label>
+	<input type="text" required name="lname">
+	<button type="submit" class="btn" name="edit_lname">Submit</button>
+</form>
+<form method="post" action="profileEdit.php">
+	<label>Change your username here</label>
+	<input type="text" required name="username">
+	<button type="submit" class="btn" name="edit_username">Submit</button>
+</form>
+<form method="post" action="profileEdit.php">
+	<label>Change your password here</label>
+	<input type="text" required name="password">
+	<button type="submit" class="btn" name="edit_password">Submit</button>
+</form>
+<form method="post" action="profileEdit.php">
+	<label>Change your email here</label>
+	<input type="text" required name="email">
+	<button type="submit" class="btn" name="edit_email">Submit</button>
+</form>
+</br>
+</br>
+</br>
 
-// DISPLAYS THE STUDENT TABLE
 
-$student_table = "";
+<?php echo $payment_table; ?>
+</br>
+<form method="post" action="profileEdit.php">
+	<label>Card Type</label>
+	<select required name = "card_type">
+		<option value = ""></option>
+		<option value = "VISA">VISA</option>
+		<option value = "Mastercard">Mastercard</option>
+		<option value = "American Express">American Express</option>
+		<option value = "Discover">Discover</option>
+	</select>
+	</br>
+	<label>Card number</label>
+	<input type="text" required name="card_num">
+	</br>
+	<label>CVV</label>
+	<input type="text" required name="cvv">
+	</br>
+	<label>Zip code</label>
+	<input type="text" required name="zip_code">
+	</br>
+	<label>Expiry date</label>
+	<input type = "date" required name = "exp_date">
+	</br>
+	<button type="submit" class="btn" name="add_payment">Add payment information</button>
+</form>
 
-$payload = "SELECT student_id, student_fname, student_lname, student_username, student_password, student_email FROM student WHERE student_id = '".$_SESSION['student_id']."'";
-$result = mysqli_query($link, $payload);
-
-if (mysqli_num_rows($result) > 0) {
-	$row = mysqli_fetch_assoc($result);
-	$student_table .= "<table><tr><th>Your name</th><th>Your username</th><th>Your password</th><th>Your email</th></tr>";
-	$student_table .= "<tr><td>".$row["student_fname"]." ".$row["student_lname"]."</td><td>".$row["student_username"]."</td><td>".$row["student_password"]."</td><td>".$row["student_email"]."</td></tr>";
-	$student_table .= "</table>";
-}
-else 
-{
-	$student_table .= "<table><tr><th>Student name</th><th>Student username</th><th>Student password</th><th>Student email</th></tr></table>";
-	$student_table .= "<p>0 results</p>";
-}
-
-// DISPLAYS THE PAYMENT TABLE
-
-$payment_table = "";
-
-$payload = "SELECT payment_id, card_type, card_num, CVV, zip_code, exp_date FROM payment_info WHERE student_id = '".$_SESSION['student_id']."'";
-$result = mysqli_query($link, $payload);
-
-if (mysqli_num_rows($result) > 0) {
-	$payment_table .= "<table><tr><th>Payment ID</th><th>Card type</th><th>Card number</th><th>CVV</th><th>Zip code</th><th>Expiry date</th></tr>";
-	while ($row = mysqli_fetch_assoc($result)) {
-		$payment_table .= "<tr><td>".$row["payment_id"]."</td><td>".$row["card_type"]."</td><td>".$row["card_num"]."</td><td>".$row["CVV"]."</td><td>".$row["zip_code"]."</td><td>".$row["exp_date"]."</td></tr>";
-	}
-	$payment_table .= "</table>";
-}
-else 
-{
-	$payment_table .= "<table><tr><th>Payment ID</th><th>Card type</th><th>Card number</th><th>CVV</th><th>Zip code</th><th>Expiry date</th></tr></table>";
-	$payment_table .= "<p>0 results</p>";
-}
-
-// DISPLAY PAYMENT IDS FOR DROP DOWN (DYNAMIC)
-
-$payload = "SELECT payment_id FROM payment_info WHERE student_id = '".$_SESSION['student_id']."'";
-$display_paymentID = mysqli_query($link, $payload);
-
-// CHANGING PROFILE FIELDS
-
-if (isset($_POST['edit_fname'])) {
-	$fname = mysqli_real_escape_string($link, $_POST['fname']);
-	$payload = "UPDATE student SET student_fname = '$fname' WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	header('Location: profileEdit.php');
-}
-
-if (isset($_POST['edit_lname'])) {
-	$lname = mysqli_real_escape_string($link, $_POST['lname']);
-	$payload = "UPDATE student SET student_lname = '$lname' WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	header('Location: profileEdit.php');
-}
-
-if (isset($_POST['edit_username'])) {
-	$username = mysqli_real_escape_string($link, $_POST['username']);
-	$payload = "UPDATE student SET student_username = '$username' WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	$_SESSION['username'] = $username;
-	header('Location: profileEdit.php');
-	
-}
-
-if (isset($_POST['edit_password'])) {
-	$password = mysqli_real_escape_string($link, $_POST['password']);
-	$payload = "UPDATE student SET student_password = '$password' WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	header('Location: profileEdit.php');
-}
-
-if (isset($_POST['edit_email'])) {
-	$email = mysqli_real_escape_string($link, $_POST['email']);
-	$payload = "UPDATE student SET student_email = '$email' WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	header('Location: profileEdit.php');
-}
-
-// ADDING PAYMENT INFORMATION
-
-if (isset($_POST['add_payment'])) {
-	$card_type = mysqli_real_escape_string($link, $_POST['card_type']);
-	$card_num = mysqli_real_escape_string($link, $_POST['card_num']);
-	$cvv = mysqli_real_escape_string($link, $_POST['cvv']);
-	$zip_code = mysqli_real_escape_string($link, $_POST['zip_code']);
-	$expiry = date('Y-m-d', strtotime($_POST['exp_date']));
-	
-	$error = False;
-	
-	$errorMessage = "";
-	
-	if (!is_numeric($card_num) && !empty($card_num))
-	{
-		$errorMessage.="Card number must be digits\\n";
-		$error = True;
-	}
-	if (!is_numeric($cvv) && !empty($cvv))
-	{
-		$errorMessage.="CVV must be digits\\n";
-		$error = True;
-	}
-	if (!is_numeric($zip_code) && !empty($zip_code))
-	{
-		$errorMessage.="Zip code must be digits\\n";
-		$error = True;
-	}
-	
-	echo '<script type="text/javascript">
-		   window.onload = function () { alert("'.$errorMessage.'"); } 
-			</script>';
-	
-	if (!$error)
-	{
-		$payload = "SELECT * FROM payment_info WHERE card_num = '$card_num'";
-		$execute = mysqli_query($link, $payload);
-	
-		if (mysqli_num_rows($execute) > 0) 
-		{
-			echo '<script type="text/javascript">
-		   window.onload = function () { alert("Sorry, this card number already exists"); } 
-			</script>';	
-			$error = True;
-		}
-	}
-	
-	if (!$error)
-	{
-		$payload = "INSERT INTO payment_info (student_id, card_type, card_num, CVV, zip_code, exp_date) VALUES('".$_SESSION['student_id']."',
-		'$card_type','$card_num','$cvv','$zip_code','$expiry')";
-
-		if (!mysqli_query($link, $payload))
-		{
-			echo("Error description: " . mysqli_error($link));
-		}
-		
-		header('Location: profileEdit.php');
-	}
-}
-
-// DELETING PAYMENT ID
-
-if (isset($_POST['delete_payment_id'])) {
-	$to_delete_id = mysqli_real_escape_string($link, $_POST['selected_paymentID']);
-	
-	$payload = "DELETE FROM payment_info WHERE payment_id = '$to_delete_id'";
-	$execute = mysqli_query($link, $payload);
-	
-	header('Location: profileEdit.php');
-}
-
-// DELETING ACCOUNT
-
-if (isset($_POST['delete_account'])) {
-	$payload = "DELETE FROM student WHERE student_id = '".$_SESSION['student_id']."'";
-	$execute = mysqli_query($link, $payload);
-	
-	session_reset();
-	echo "<p>Reset session</p>";
-	header('Location: login.php');
-}
-
-mysqli_close($link);
-
-?>
+</br>
+</br>
+</br>
+<form method="post" action="profileEdit.php">
+	<label>Select a payment ID to delete</label>
+	<select required name = "selected_paymentID">
+		<option value = ""></option>
+		<?php
+			while ($indv_paymentID = mysqli_fetch_array($display_paymentID, MYSQLI_NUM)):;
+		?>
+		<option value = "<?php echo $indv_paymentID[0];?>">
+			<?php echo $indv_paymentID[0]; ?>
+		</option>
+		<?php
+			endwhile;
+		?>
+	</select>
+	<input name="delete_payment_id" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
+</form>
+</br>
+</br>
+</br>
+<form method="post" action="profileEdit.php">
+	<label>Delete your account by pressing this button</label>
+	<input name="delete_account" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
+</form>
+</br>
+</br>
+<p>Click here to go back to the home page <a href = "userHomePage.php">Click here</a></p>
+<form method="post" action="login.php">
+	<button type="submit" class="btn" name="signout">Signout</button>
+</form>
+</body>
+</html>
