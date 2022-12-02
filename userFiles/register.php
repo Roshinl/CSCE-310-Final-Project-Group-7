748a128a4f0ab9f0bd39d48c70170d67ad8137d1
clintonyau <79611899+clintonyau@users.noreply.github.com>
2022-12-02 17:45:20 -0600

    Formatting added

--- userFiles/register.php
+++ userFiles/register.php
@@ -7,18 +7,23 @@
 <form method="post" action="register.php">
 	<label>Username</label>
 	<input type="text" required name="username">
+	
 	</br>
 	<label>Password</label>
 	<input type="text" required name="password">
+	
 	</br>
 	<label>First name</label>
 	<input type="text" required name="fname">
+	
 	</br>
 	<label>Last name</label>
 	<input type="text" required name="lname">
+	
 	</br>
 	<label>Email</label>
 	<input type="text" required name="email">
+	
 	</br>
 	<button type="submit" class="btn" name="register_user">Register</button>
 </form>
