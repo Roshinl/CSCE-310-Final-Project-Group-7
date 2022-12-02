b55685bd03589c4aa5d9adfed6daf8e40ad0ae43
clintonyau <79611899+clintonyau@users.noreply.github.com>
2022-12-02 17:46:07 -0600

    Formatting changes

--- userFiles/login.php
+++ userFiles/login.php
@@ -7,11 +7,14 @@
 <form method="post" action="login.php">
 	<label>Username</label>
 	<input type="text" required name="username">
+	
 	</br>
 	<label>Password</label>
 	<input type="text" required name="password">
+	
 	</br>
 	<button type="submit" class="btn" name="login_user">Login</button>
+	
 	</br>
 	</br>
 	<p>Register here if you don't have an account: <a href ="register.php">Register</a></p>
