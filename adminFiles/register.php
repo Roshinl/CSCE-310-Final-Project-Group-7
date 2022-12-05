9feed7f80fd0bccd6df053963a4155b5325ee79c
Roshin Lal <66693974+Roshinl@users.noreply.github.com>
2022-12-05 12:23:56 -0600

    Admin can register an account

---
+++ adminFiles/register.php
@@ -0,0 +1,29 @@
+<?php include ('loginRegBackend.php')?>
+<!DOCTYPE html>
+<html>
+<head><title>Register Admin</title></head>
+<body>
+<head>REGISTER</head>
+<form method="post" action="register.php">
+	<label>Username</label>
+	<input type="text" required name="username">
+	</br>
+	<label>Password</label>
+	<input type="text" required name="password">
+	</br>
+	<label>First name</label>
+	<input type="text" required name="fname">
+	</br>
+	<label>Last name</label>
+	<input type="text" required name="lname">
+	</br>
+	<label>Email</label>
+	<input type="text" required name="email">
+	</br>
+	<button type="submit" class="btn" name="register_admin">Register</button>
+</form>
+</br>
+</br>
+<p>Already have an account? Login here: <a href ="login.php">Login</a></p>
+</body>
+</html>
