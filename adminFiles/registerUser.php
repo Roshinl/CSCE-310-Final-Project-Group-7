c69a614ed5e378b98ff8eea9551df2aebd69723c
Roshin Lal <66693974+Roshinl@users.noreply.github.com>
2022-12-05 12:24:38 -0600

    Admin can register student/tutor accounts

--- adminFiles/registerUser.php
+++ adminFiles/registerUser.php
@@ -3,7 +3,7 @@
 <html>
 <head><title>Register</title></head>
 <body>
-<head>REGISTER</head>
+<head>REGISTER A STUDENT HERE</head>
 <form method="post" action="register.php">
 	<label>Username</label>
 	<input type="text" required name="username">
@@ -25,10 +25,38 @@
 	<input type="text" required name="email">
 	
 	</br>
-	<button type="submit" class="btn" name="register_user">Register</button>
+	<button type="submit" class="btn" name="register_student">Register</button>
+</form>
+
+<br><br>
+
+<head>REGISTER A TUTOR HERE</head>
+<form method="post" action="register.php">
+	<label>Username</label>
+	<input type="text" required name="username">
+	
+	</br>
+	<label>Password</label>
+	<input type="text" required name="password">
+	
+	</br>
+	<label>First name</label>
+	<input type="text" required name="fname">
+	
+	</br>
+	<label>Last name</label>
+	<input type="text" required name="lname">
+	
+	</br>
+	<label>Email</label>
+	<input type="text" required name="email">
+	
+	</br>
+	<button type="submit" class="btn" name="register_tutor">Register</button>
 </form>
 </br>
 </br>
+
 </body>
 
 <!-- PREVENTS ANNOYING RESUBMIT FORM MESSAGE FROM SHOWING UP -->
