07b06c3c2bcded61c3455ecc97e2a1ae11e7c0a7
Roshin Lal <66693974+Roshinl@users.noreply.github.com>
2022-12-06 19:56:43 -0600

    Update profileEditBackend.php

--- adminFiles/profileEditBackend.php
+++ adminFiles/profileEditBackend.php
@@ -55,11 +55,6 @@ else
 	$payment_table .= "<p>0 results</p>";
 }
 
-// DISPLAY PAYMENT IDS FOR DROP DOWN (DYNAMIC)
-
-$payload = "SELECT payment_id FROM payment_info WHERE user_id = '".$_SESSION['user_id']."'";
-$display_paymentID = mysqli_query($link, $payload); // this is passed to HTML, which loops + prints the information
-
 // CHANGING PROFILE FIELDS
 
 if (isset($_POST['edit_fname'])) {
@@ -137,90 +132,6 @@ if (isset($_POST['edit_email'])) {
 		$execute = mysqli_query($link, $payload);
 		header('Location: profileEdit.php');
 	}
-}
-
-// ADDING PAYMENT INFORMATION
-
-if (isset($_POST['add_payment'])) {
-	$card_type = mysqli_real_escape_string($link, $_POST['card_type']);
-	$card_num = mysqli_real_escape_string($link, $_POST['card_num']);
-	$cvv = mysqli_real_escape_string($link, $_POST['cvv']);
-	$zip_code = mysqli_real_escape_string($link, $_POST['zip_code']);
-	$expiry = date('Y-m-d', strtotime($_POST['exp_date'])); // date converts the string into ready-to-insert SQL
-	
-	$error = False;
-	
-	$errorMessage = "";
-	
-	if (!is_numeric($card_num)) // checks if the card number is a number
-	{
-		$errorMessage.="Card number must be digits\\n";
-		$error = True;
-	}
-	if (!is_numeric($cvv))
-	{
-		$errorMessage.="CVV must be digits\\n";
-		$error = True;
-	}
-	if (!is_numeric($zip_code))
-	{
-		$errorMessage.="Zip code must be digits\\n";
-		$error = True;
-	}
-	
-	if ($error) {
-		echo '<script type="text/javascript">
-			   window.onload = function () { alert("'.$errorMessage.'"); } 
-				</script>';
-	}
-	
-	else
-	{
-		$payload = "SELECT * FROM payment_info WHERE card_num = '$card_num'";
-		$execute = mysqli_query($link, $payload);
-	
-		if (mysqli_num_rows($execute) > 0) 
-		{
-			echo '<script type="text/javascript">
-		   window.onload = function () { alert("Sorry, this card number already exists"); } 
-			</script>';	
-			$error = True;
-		}
-	}
-	
-	if (!$error)
-	{
-		$payload = "INSERT INTO payment_info (user_id, card_type, card_num, CVV, zip_code, exp_date) VALUES('".$_SESSION['user_id']."',
-		'$card_type','$card_num','$cvv','$zip_code','$expiry')";
-
-		if (!mysqli_query($link, $payload))
-		{
-			echo("Error description: " . mysqli_error($link));
-		}
-		
-		header('Location: profileEdit.php');
-	}
-}
-
-// DELETING PAYMENT ID
-
-if (isset($_POST['delete_payment_id'])) {
-	$to_delete_id = mysqli_real_escape_string($link, $_POST['selected_paymentID']);
-	
-	$payload = "DELETE FROM payment_info WHERE payment_id = '$to_delete_id'";
-	$execute = mysqli_query($link, $payload);
-	
-	$payload = "SELECT payment_id, card_type, card_num, CVV, zip_code, exp_date FROM payment_info WHERE user_id = '".$_SESSION['user_id']."'";
-	$result = mysqli_query($link, $payload);
-	
-	// Checks if the user has any payment information left, if they don't, we set their has_payment to 0
-	if (mysqli_num_rows($result) <= 0)
-	{
-		$payload = "UPDATE student SET has_payment = 0 WHERE user_id = '".$_SESSION['user_id']."'";
-		$execute = mysqli_query($link, $payload);
-	}
-	
-	header('Location: profileEdit.php');
 }
 
 // DELETING ACCOUNT
